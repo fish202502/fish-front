d8dcdd919de93904179ec128077c6447c395d05d
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-13 17:55:20 +0900

    exercise header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 
 import './App.css'
 import FinancialManager from './components/financial/FinancialManager'
-
-
-
+import Header from './components/layout/Header'
 
 function App() {
 
-
   return (
-    <>
-    
-    <FinancialManager/>
- 
+    <>    
+   <Header/>   
+   <FinancialManager/>
     </>
   )
 }
