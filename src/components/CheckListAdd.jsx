5fdca2b96aca928bf648b469f9bd866a717b4b5e
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-15 15:47:23 +0900

    initialize

---
+++ src/components/CheckListAdd.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CheckListAdd = () => {
+  return (
+    <React.Fragment>
+    <div>체크리스트 추가</div>
+    <input type="text" placeholder='항목 추가'/>
+    <button>추가</button>
+    </React.Fragment>
+  )
+}
+
+export default CheckListAdd
---
+++ src/components/CheckListManager.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CheckListManager = () => {
+  return (
+    <h1>체크리스트 폼</h1>
+  )
+}
+
+export default CheckListManager
