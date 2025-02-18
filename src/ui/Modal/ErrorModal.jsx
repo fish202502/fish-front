3d609b83297fd3f4a36ee24f377c6bc9ef5af3a5
chchch928 <kbanda@naver.com>
2025-02-19 00:03:11 +0900

    #15_에러모달 css 추가

--- src/ui/Modal/ErrorModal.jsx
+++ src/ui/Modal/ErrorModal.jsx
@@ -4,11 +4,14 @@ import styles from './ErrorModal.module.css'
 const ErrorModal = ({title,message,onClose}) => {
   return (
     <>
-      <div onClick={onClose} className={styles.backdrop}></div>
+      <div onClick={onClose} className={styles.backdrop}>
+
       <div className={styles.modal}>
-        <h2>{title}</h2>
-        <p>{message}</p>
-        <button onClick={onClose}>확인</button>
+        <h2 className={styles.title}>{title}</h2>
+        <p className ={styles.message}>{message}</p>
+        <button className={styles.button} onClick={onClose}>확인</button>
+      </div>
+
       </div>
     </>
   );
