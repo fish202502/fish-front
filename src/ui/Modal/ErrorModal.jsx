68dce7397ae8ff70fbbd8081c297f513b6db3ca7
chchch928 <kbanda@naver.com>
2025-02-18 15:25:07 +0900

    #9_모달 구조 변경 및 css 변경

--- src/ui/Modal/ErrorModal.jsx
+++ src/ui/Modal/ErrorModal.jsx
@@ -3,14 +3,18 @@ import styles from './ErrorModal.module.css'
 
 const ErrorModal = ({title,message,onClose}) => {
     return (
-        <>
-            <div onClick={onClose} className={styles.backdrop}></div>
+
+            <div onClick={onClose} className={styles.backdrop}>
+
             <div className={styles.modal}>
                 <h2>{title}</h2>
                 <p>{message}</p>
                 <button onClick={onClose}>확인</button>
             </div>
-        </>
+
+
+            </div>
+
     );
 };
 
