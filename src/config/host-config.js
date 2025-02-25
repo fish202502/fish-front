cf6282e18fd98dfd3e5ca72d705f4cbb190aa7e6
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-25 17:13:15 +0900

    host-config 추가

---
+++ src/config/host-config.js
@@ -0,0 +1,27 @@
+
+// 백엔드 로컬서버의 포트번호
+const LOCAL_PORT = 8999;
+
+const clientHostName = window.location.hostname;
+
+// 백엔드 호스트 조건부 처리
+let backendHostName;
+
+if (clientHostName === 'localhost') {
+  backendHostName = `http://localhost:${LOCAL_PORT}`;
+} 
+
+// 기본 백엔드 주소 저장
+const API_BASE_URL = `${backendHostName}/api/fish`;
+
+// API별 메인엔드포인트
+const EXPENSE = '/expense';
+const CHECK= '/check';
+const SCHEDULE = '/schedule'
+
+
+// 만약에 클라이언트가 localhost면 
+// http://localhost:9000/api/events
+export const EXPENSE_API_URL = `${API_BASE_URL}${EXPENSE}`;
+export const CHECK_API_URL = `${API_BASE_URL}${CHECK}`;
+export const SCHEDULE_API_URL = `${API_BASE_URL}${SCHEDULE}`;
