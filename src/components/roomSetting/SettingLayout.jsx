1a7af216844c660ecf45849c1b11afbd0ec0aec8
chchch928 <kbanda@naver.com>
2025-02-27 17:51:09 +0900

    #39_setting scss 수정 및 각 채팅을 제외한 border-radius 설정

--- src/components/roomSetting/SettingLayout.jsx
+++ src/components/roomSetting/SettingLayout.jsx
@@ -4,9 +4,13 @@ import ShowUrl from "../room/ShowUrl";
 import styles from "./SettingLayout.module.scss"
 const SettingLayout = () => {
   return (
+    <div className={styles.FullSettingFrame}>
     <div className={styles.SettingFrame}>
       <Setting />
       <ShowUrl />
+    </div>
+
+
     </div>
   );
 };
